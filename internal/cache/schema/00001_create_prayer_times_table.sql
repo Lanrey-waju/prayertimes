739acdd57f1ff648ec3b284a1851fe296633f49d
Abdulmumin Akinde <lanreywaju97@gmail.com>
2025-03-22 00:51:07 +0100

    chore: move migrations to cache package

---
+++ internal/cache/schema/00001_create_prayer_times_table.sql
@@ -0,0 +1,20 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS prayer_times (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    city TEXT NOT NULL,
+    date DATE NOT NULL,
+    fajr TEXT NOT NULL,
+    dhuhr TEXT NOT NULL,
+    asr TEXT NOT NULL,
+    maghrib TEXT NOT NULL,
+    isha TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE(city, date)  -- Prevent duplicates per city per day
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE prayer_times;
+-- +goose StatementEnd
