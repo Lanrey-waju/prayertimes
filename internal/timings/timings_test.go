5636dcd63d8d297d01853b69b0139f4de8a19ed8
Abdulmumin Akinde <lanreywaju97@gmail.com>
2025-03-29 01:06:51 +0100

    refactor tests with table-driven approach

--- internal/timings/timings_test.go
+++ internal/timings/timings_test.go
@@ -5,7 +5,21 @@ import (
 )
 
 func TestIsPrayerTimeOver(t *testing.T) {
-	if isPrayerTimeOver("05:40", "05:46") != false {
-		t.Errorf("Expected false, got true")
+	tests := map[string]struct {
+		currentTime string
+		prayerTime  string
+		want        bool
+	}{
+		"prayer time not over": {currentTime: "05:06", prayerTime: "05:40", want: false},
+		"prayer time over":     {currentTime: "13:00", prayerTime: "12:58", want: true},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := isPrayerTimeOver(tc.prayerTime, tc.currentTime)
+			if tc.want != got {
+				t.Fatalf("expected %v, got %v", tc.want, got)
+			}
+		})
 	}
 }
